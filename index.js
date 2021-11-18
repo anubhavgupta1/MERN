3b6ba06df163422e1d4ad394cb506e9535685c49
Anubhav Gupta <sbansal792@gmail.com>
2021-11-19 04:24:07 +0530

    4th Commit : Static Files and Folders Setup

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const port = 8000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static(__dirname +'/assets'));
 app.use(expressLayouts);
-//app.set('layout extractStyles', true);
-//app.set('layout extractScripts', true);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use('/', require('./routes/index'));
